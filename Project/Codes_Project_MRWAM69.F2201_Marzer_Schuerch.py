e859ab56254560b8691d3418a079d828137e4f20
dario.schuerch <dario.schuerch@bluewin.ch>
2022-04-28 00:37:03 +0200

    my_changes

--- Project/Codes_Project_MRWAM69.F2201_Marzer_Schuerch.py
+++ Project/Codes_Project_MRWAM69.F2201_Marzer_Schuerch.py
@@ -3,7 +3,8 @@ import yfinance as yf
 from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
-
+from pandas_datareader import data as pdr
+from scipy.stats import norm
 
 start = datetime(2004,1,1)
 end = datetime.today()
@@ -13,9 +14,9 @@ SMI.to_csv("SMI_ALL.csv")
 
 
 #Gleitender Mittelwert 50 / 200 2004 - 2022
-SMI["MA50"] = SMI["Close"].rolling(50).mean()
-SMI["MA200"] = SMI["Close"].rolling(200).mean()
-SMI["Close"].plot(figsize = (15,4))
+SMI["MA50"] = SMI["Adj Close"].rolling(50).mean()
+SMI["MA200"] = SMI["Adj Close"].rolling(200).mean()
+SMI["Adj Close"].plot(figsize = (15,4))
 SMI["MA50"].plot()
 SMI["MA200"].plot()
 plt.grid(which="major", color="k", linestyle="-.", linewidth=0.3)
@@ -33,9 +34,9 @@ SMI_08= yf.download("^SSMI", start, end)
 SMI_08.to_csv("SMI_08.csv")
 
 #plot
-SMI_08 ["MA50"] = SMI_08["Close"].rolling(50).mean()
-SMI_08 ["MA100"] = SMI_08["Close"].rolling(100).mean()
-SMI_08["Close"].plot(figsize = (15,4))
+SMI_08 ["MA50"] = SMI_08["Adj Close"].rolling(50).mean()
+SMI_08 ["MA100"] = SMI_08["Adj Close"].rolling(100).mean()
+SMI_08["Adj Close"].plot(figsize = (15,4))
 SMI_08["MA50"].plot()
 SMI_08["MA100"].plot()
 plt.grid(which="major", color="k", linestyle="-.", linewidth=0.3)
@@ -54,9 +55,9 @@ SMI_19= yf.download("^SSMI", start, end)
 SMI_19.to_csv("SMI_19.csv")
 
 #plot
-SMI_19["MA50"] = SMI_19["Close"].rolling(50).mean()
-SMI_19["MA100"] = SMI_19["Close"].rolling(100).mean()
-SMI_19["Close"].plot(figsize = (15,4))
+SMI_19["MA50"] = SMI_19["Adj Close"].rolling(50).mean()
+SMI_19["MA100"] = SMI_19["Adj Close"].rolling(100).mean()
+SMI_19["Adj Close"].plot(figsize = (15,4))
 SMI_19["MA50"].plot()
 SMI_19["MA100"].plot()
 plt.grid(which="major", color="k", linestyle="-.", linewidth=0.3)
@@ -68,10 +69,23 @@ plt.title("SMI 2019-2021: Moving Average of a 50 and 100 Day Base", fontsize = 2
 
 # gibt den Mittelwert SMI 08 / 19 aus
 
-mean_SMI_08 = SMI_08["Close"].mean()
-mean_SMI_19 = SMI_19["Close"].mean()
+mean_SMI_08 = SMI_08["Adj Close"].mean()
+mean_SMI_19 = SMI_19["Adj Close"].mean()
 print(mean_SMI_08)
 print(mean_SMI_19)
+
+## Volatility Clusterting SMI 2004-2022
+#Calculate Pct Change & Log Return (steady return)
+df_04 = pd.read_csv("SMI_ALL.csv",sep = ",", index_col=0)
+df_04["Pct Change"]= df_04["Adj Close"].pct_change()
+df_04["Log Return"]= np.log(df_04["Adj Close"]/df_04["Adj Close"].shift(1))
+df_04 =df_04[1:] #Remove the first observation NaN
+df_04.head()
+#ploting volatility clustering
+df_04["Log Return"].plot(figsize = (20,7))
+plt.title("Volatility Clustering of SMI 2004-2022",  fontsize = 30)
+plt.ylabel("Percent", fontsize = 25)
+plt.xlabel("Date",  fontsize = 25)
 
 
 # Comparison Volatility SMI 2008-2010 and SMI 2019-2021
@@ -79,40 +93,55 @@ print(mean_SMI_19)
 #data 08: calculate pct change & log return
 
 df_08 = pd.read_csv("SMI_08.csv",sep = ",", index_col=0)
-df_08["Pct Change"]= df_08["Close"].pct_change()
-df_08["Log Return"]= np.log(df_08["Close"]/df_08["Close"].shift(1))
+df_08["Pct Change"]= df_08["Adj Close"].pct_change()
+df_08["Log Return"]= np.log(df_08["Adj Close"]/df_08["Adj Close"].shift(1))
+df_08 =df_08[1:] #Remove the first observation (NaN)
 df_08.head()
 
 #data 19: calculate pct change & log return
 df_19 = pd.read_csv("SMI_19.csv",sep = ",", index_col=0)
-df_19["Pct Change"]= df_19["Close"].pct_change()
-df_19["Log Return"]= np.log(df_19["Close"]/df_19["Close"].shift(1))
+df_19["Pct Change"]= df_19["Adj Close"].pct_change()
+df_19["Log Return"]= np.log(df_19["Adj Close"]/df_19["Adj Close"].shift(1))
+df_19 =df_19[1:] #Remove the first observation (NaN)
 df_19.head()
 
-# Volatilität SMI über 2 Börsenjahre in den jeweiligen Krisen (252 *3)
-ln_SMI_08= df_08["Log Return"].std()
-ln_SMI_19= df_19["Log Return"].std()
-std_SMI_08 = ln_SMI_08 * np.sqrt(504) * 100
-std_SMI_19 = ln_SMI_19 * np.sqrt(504) * 100
+# Volatilität SMI über 3 Börsenjahre in den jeweiligen Krisen (252 *3)
+std_SMI_08= df_08["Log Return"].std()
+std_SMI_19= df_19["Log Return"].std()
+std_SMI_08_3Y = std_SMI_08 * np.sqrt(756) * 100
+std_SMI_19_3Y = std_SMI_19 * np.sqrt(756) * 100
 
-print(std_SMI_08)
-print(std_SMI_19)
+print(std_SMI_08_3Y)
+print(std_SMI_19_3Y)
 
-#plot
+
+#plot volatility histogram
 df_08["Log Return"].hist(bins = 100, label = "SMI 2008-2010", alpha = 0.5, figsize = (15,7))
 df_19["Log Return"].hist(bins = 100, label = "SMI 2019-2021", alpha = 0.5)
 plt.legend()
 plt.title("Volatility SMI 2008-2010 and SMI 2019-2021", fontsize = 30)
 
-#VaR 5 Titel 2008-2010  during Financial Crisis
-from pandas_datareader import data as pdr
-from scipy.stats import norm
+#Übersicht portfolio
+portfolio = ["NOVN.SW", "UBSG.SW", "NESN.SW", "CSGN.SW", "ABBN.SW"]
+start = datetime(2004,1,1)
+end = datetime.today()
+df = pdr.get_data_yahoo(portfolio, start, end)
+plt.figure(figsize = (15,7))
+plt.grid(which="major", color='k', linestyle='-.', linewidth=0.3)
+plt.ylabel(" CHF", fontsize =15)
+plt.xlabel("Date", fontsize =15)
+labels = ["NOVN.SW", "UBSG.SW", "NESN.SW", "CSGN.SW", "ABBN.SW"]
+plt.plot(df ["Adj Close"], label = labels)
+plt.title("Price Changes SMI sample Portfolio", fontsize = 25)
+plt.legend()
+
+#Max Portfolio loss  5 Titel 2008-2010  during Financial Crisis
 portfolio_08 = ["NOVN.SW", "UBSG.SW", "NESN.SW", "CSGN.SW", "ABBN.SW"]
 weights_08 = np.array([.2, .2, .2, .2, .2])
 investment_08 = 1000000
 start_08 = datetime(2008,1,1)
 end_08 = datetime(2010,12,31)
-df_08 = pdr.get_data_yahoo(portfolio_08, start_08, end_08) ["Close"]
+df_08 = pdr.get_data_yahoo(portfolio_08, start_08, end_08) ["Adj Close"]
 returns_08 = df_08.pct_change()
 
 # Generate Var-Cov matrix (Kovarianzmatrix)
@@ -154,10 +183,32 @@ plt.xlabel("Days", fontsize = 15)
 plt.ylabel("Max. portfolio loss in Mio (CHF)", fontsize = 15)
 plt.xlim(0,800)
 plt.ylim(0,1_000_000)
-plt.title("Max portfolio loss (VaR) during Financial Crisis 2008-2010", fontsize = 25)
+plt.title("VaR sample portfolio during Financial Crisis 2008-2010", fontsize = 25)
 plt.plot(Var_array_08, "b")
 plt.grid(which="major", color="k", linestyle="-.", linewidth=0.3)
 
+#Calculating VaR 2008-2010
+
+returns_19 = returns_19.fillna(0.0)
+portfolio_returns_19 = returns_19.iloc[-days_19:].dot(weights_19)
+
+VaR_19 = np.percentile(portfolio_returns_19, 100 * (conf_level_19)) * investment_19
+print(VaR_19)  # max loss with a conf level of 95% is 18.4k
+
+#plot
+portfolio_returns_19_ = returns_19.fillna(0.0).iloc[-days_19:].dot(weights_19)
+
+portfolio_VaR_19 = VaR_19
+portfolio_VaR_return_19 = portfolio_VaR_19 / investment_19
+
+plt.figure(figsize=(15,7))
+plt.hist(portfolio_returns_19, bins= 50)
+plt.axvline(portfolio_VaR_return_19, color="r", linestyle="solid")
+plt.legend(["VaR for alpha = 5", "Historical Returns SMI Portfolio" ])
+plt.title("Value at Risk (VaR) SMI Portfolio 2019-2021", fontsize = 25)
+plt.xlabel("Return", fontsize = 20)
+plt.ylabel("Observation Frequency", fontsize = 20)
+plt.grid(which="major", color="k", linestyle="-.", linewidth=0.3)
 
 #Checking distributions of equities against normal distribution
 import matplotlib.pyplot as plt
@@ -203,7 +254,7 @@ weights_19 = np.array([.2, .2, .2, .2, .2])
 investment_19 = 1000000
 start_19 = datetime(2019,1,1)
 end_19 = datetime(2021,12,31)
-df_19 = pdr.get_data_yahoo(portfolio_19, start_19, end_19) ["Close"]
+df_19 = pdr.get_data_yahoo(portfolio_19, start_19, end_19) ["Adj Close"]
 returns_19 = df_19.pct_change()
 #print(returns)
 
@@ -233,7 +284,6 @@ Var = investment_19 - var_cutoff_19
 #print(Var)
 
 #Calculate VaR over 3 Years
-import matplotlib.pyplot as plt
 Var_array_19= []
 days_19 = int(756)
 for x in range(1, days_19+1):
@@ -245,13 +295,13 @@ plt.xlabel("Days", fontsize = 15)
 plt.ylabel("Max. portfolio loss (CHF)", fontsize = 15)
 plt.xlim(0,800)
 plt.ylim(0,700_000)
-plt.title("Max portfolio loss (VaR) during COVID Pandedmic 2019-2021", fontsize = 25)
+plt.title("VaR sample portfolio during COVID Pandedmic 2019-2021", fontsize = 25)
 plt.plot(Var_array_19, "b")
 plt.grid(which="major", color="k", linestyle="-.", linewidth=0.3)
 
+
+
 #Checking distributions of equities against normal distribution
-import matplotlib.pyplot as plt
-import scipy as scipy
 
 returns_19["NOVN.SW"].hist(bins = 100, label = "NOVN.SW", alpha = 0.5, figsize = (10,4))
 x = np.linspace(mean_portfolio_19 - 3* std_portfolio_19, mean_portfolio_19 +3 *std_portfolio_19,100)
@@ -291,14 +341,14 @@ plt.legend()
 #Sharp ratio Portfolio 2008-2010
 returns_08 = np.sum(np.multiply(mean_returns_08, weights_08)) * 504
 std_08 = std_portfolio_08
-risk_free_return_08 = int(0.018) # Annahme
+risk_free_return_08 = int(0.018) # assumption
 sharpe_ratio_08= (mean_portfolio_08 - risk_free_return_08) / std_08
 print(sharpe_ratio_08)
 
 #Sharp ratio Portfolio 2019-2021
 returns_19 = np.sum(np.multiply(mean_returns_08, weights_08)) * 504
 std_19 = std_portfolio_19
-risk_free_return_19 = 0 # Annahme
+risk_free_return_19 = 0 # assumption
 sharpe_ratio_19= (mean_portfolio_19 - risk_free_return_19) / std_19
 print(sharpe_ratio_19)
 
@@ -315,5 +365,7 @@ plt.title("Sharpe Ratio Portfolio 2008-2010 vs 2019-2021", fontsize = (25))
 plt.legend(["Portfolio 2008-2010","Portfolio 2019-2021"])
 plt.xticks(x, fontsize = (15))
 plt.legend(["Portfolio 2019-2021"])
+
+
 
 
